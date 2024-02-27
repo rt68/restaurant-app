44232e11727890cb00c271535c51ef90d7a56ebc
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-02-27 16:15:34 -0500

    config updated

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 require("./config/database");
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const ensureLoggedIn = require('./config/ensureLoggedIn');
 
@@ -19,7 +19,7 @@ app.use(require('./config/checkToken'));
 
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 //"Catch All" Route
