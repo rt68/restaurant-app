19e8c09da1a78201bc84b4cd93d3bf04b8731479
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-02-27 21:33:03 -0500

    linked all 3 categories and subs

--- frontend/src/components/MenuListItem/MenuListItem.js
+++ frontend/src/components/MenuListItem/MenuListItem.js
@@ -1,7 +1,8 @@
 import styles from "./MenuListItem.module.css";
-
+import { Link } from 'react-router-dom';
 export default function MenuListItem({ menuItem, handleAddToOrder }) {
   return (
+    <Link to={`/menu/category/${menuItem._id}`} className="menu-item-link">
     <div className={styles.MenuListItem}>
       <div className={styles.img + " " + "flex-ctr-ctr"}>
         {menuItem.img}
@@ -9,6 +10,7 @@ export default function MenuListItem({ menuItem, handleAddToOrder }) {
       <div className={styles.name}>{menuItem.name}</div>
       <div className={styles.buy}>
         <span>${menuItem.price.toFixed(2)}</span>
+       
         <button
           className="btn-sm"
           onClick={() => handleAddToOrder(menuItem._id)}
@@ -17,5 +19,6 @@ export default function MenuListItem({ menuItem, handleAddToOrder }) {
         </button>
       </div>
     </div>
+    </Link>
   );
 }
