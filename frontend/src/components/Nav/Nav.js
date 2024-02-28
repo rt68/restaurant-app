7eb0b00aa787841722765172d3034b5c78367d24
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-02-28 10:22:59 -0500

    Working App.js with proper routes

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -1,3 +1,29 @@
+// import { Link } from 'react-router-dom';
+// import Auth from '../../pages/Auth/Auth';
+// import UserLogOut from '../UserLogOut/UserLogOut'
+
+// export default function Nav({user, setUser}) {
+
+//     return (
+//         <nav>
+//             <Link to='/'>Ramen Time - 拉面时光</Link> &nbsp; | &nbsp;
+//             {user?(<>Hello {user.name}&nbsp; | &nbsp; <UserLogOut user={user} setUser={setUser} />
+//             <Link to="/orders/new">Cart</Link> &nbsp; | &nbsp;</>):(<>
+//                 <Link to = "/menu">Menu</Link>&nbsp; | &nbsp;
+//              <Link to="/users">Sign Up/Log In</Link>
+//             &nbsp; | &nbsp;
+//             <Link to ="/cart"> Cart </Link>       
+//             </>
+//             )}
+// {/*             
+//             <Link to="/login">Login</Link>  &nbsp; | &nbsp;
+//             <Link to="/signup">Sign Up</Link> */}
+           
+
+//         </nav>
+//     )
+// }
+
 import { Link } from 'react-router-dom';
 import Auth from '../../pages/Auth/Auth';
 import UserLogOut from '../UserLogOut/UserLogOut'
