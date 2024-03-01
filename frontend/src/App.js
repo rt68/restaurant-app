1b4f8a090574c22748558f08e6ba9c3db8199272
bristeele99 <bristeele99@gmail.com>
2024-02-29 21:41:51 -0500

    css for nav footer landing about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 //imports
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -10,11 +9,11 @@ import Nav from "./components/Nav/Nav";
 import Landing from "./pages/Landing/Landing";
 import Auth from "./pages/Auth/Auth";
 import NewOrder from "./pages/NewOrder/NewOrder";
-
 import CategoryMenu from "./pages/Menu/CategoryMenu"
 import MenuListItem from "./components/MenuListItem/MenuListItem";
-
 import OrderHistory from "./pages/OrderHistory/OrderHistory";
+import Footer from './components/Footer/Footer'
+import About from './pages/About/About'
 
 function App() {
   const [user, setUser] = useState(getUser());
@@ -39,35 +38,48 @@ function App() {
     <div className="App">
       <Nav user={user} setUser={setUser} />
       <Routes>
-        <Route path="/" element={<Landing />} />
-
+      <Route path="/" element={<Landing />} />
+      <Route path='/about' element={<About/>}/>
         {user ? (
-          // Authenticated routes
           <>
-            <Route
-              path="/orders/new"
-              element={<NewOrder user={user} setUser={setUser} />}
-            />
-            <Route
-              path="/orders"
-              element={<OrderHistory user={user} setUser={setUser} />}
-            />
 
             <Route path="/orders/new" element={<NewOrder user={user} setUser={setUser} />} />
             <Route path="/orders" element={<OrderHistory user={user} setUser={setUser} />} />
-
             <Route path="/*" element={<Navigate to="/orders/new" />} />
-
-            {/* <Route path="/menu" element={<Menu />} /> */}
           </>
         ) : (
-          // Non-authenticated route
           <>
-            <Route path="/menu" element={<Menu />} />
+            <Route path="/" element={<Landing />} />
+            <Route
+              path="/menu"
+              element={
+                <CategoryMenu
+                  user={user}
+                  categories={categoriesRef.current}
+                  activeCat={activeCat}
+                  setActiveCat={setActiveCat}
+                />
+              }
+            />
+            <Route
+              path="/menu/:categoryId"
+              element={
+                <MenuListItem
+                  menuItems={menuItems.filter(
+                    (item) => item.category.name === activeCat
+                  )} 
+                />
+              }
+            ></Route>
+            {/* <Route
+              path="/menu/:categoryId/:itemId"
+              element={<MenuItemDetail />}
+            /> */}
             <Route path="/users" element={<Auth setUser={setUser} />} />
           </>
         )}
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- frontend/src/pages/Landing/Landing.js
+++ frontend/src/pages/Landing/Landing.js
@@ -1,26 +1,28 @@
-import Footer from "../../components/Footer/Footer";
+import styles from './Landing.module.css'
+
 export default function Landing() {
   return (
     <main>
-    <h1>Welcome to Ramen Time
-    </h1>
-      <h1>We take Ramen so SERIOUSLY</h1>
-      <span>
+    <div className={styles.group}>
+      <h2 className={styles.h2}>We take Ramen so SERIOUSLY</h2>
+      <p className={styles.p}>
         Ramen is essentially a bowl of noodles and broth. It is a relatively
         simple dish on the surface, but its apparent simplicity is a serious
         part of its genius and when prepared by masters, it is a meal full of
         little subtleties, expertise and incredible depths of flavour. For
         example, depending on the soup base, choice of noodles, toppings, and
         sides, ramen can have a huge number of different variations.
-      </span>
-      <span>
+      </p>
+      <br/>
+      <img className={styles.img} src='https://res.cloudinary.com/dxh60x8dq/image/upload/v1709222355/Ramen%20Time%20Images/image_720_kjpex0.png' alt='Ramen-Bowl'/>
+      <p className={styles.p}>
         The most common types of bases are shoyu (soy sauce), shio (salt), miso
         (soybean paste) and tonkotsu (pork bone). Shoyu and Shio is usually made
         with chicken broth although the meat can vary by region. Miso originated
         in the cold Hokkaido area in the north while tonkotsu is particularly
         popular in Kyushu.
-      </span>
-      <Footer />
+      </p>
+      </div>
     </main>
   );
 }
